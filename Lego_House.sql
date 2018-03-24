fe44848a4574b5610e735abc38fda439be340003
Alek2750 <alek2750@gmail.com>
2018-03-24 12:48:45 +0100

    SQL for batabase

---
+++ Lego_House.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE  IF NOT EXISTS `useradmin`;
+
+
+USE `useradmin`;
+
+DROP TABLE IF EXISTS `order`;
+DROP TABLE IF EXISTS `Users`;
+
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(90) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `role` varchar(20) NOT NULL DEFAULT 'customer',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `order` (
+`orderID` int(11) NOT NULL AUTO_INCREMENT,
+`height` int(11) NOT NULL,
+`length` int(11) NOT NULL,
+`width` int(11) NOT NULL,
+`user_id`int(11) NOT NULL, 
+primary key (orderID),
+constraint `user_id` FOREIGN KEY (`user_id`) REFERENCES `useradmin`.`users` (`id`)
+);
+
+LOCK TABLES `Users` WRITE;
+INSERT INTO `Users` VALUES 
+(1,'jens@somewhere.com','jensen','customer'),
+(2,'ken@somewhere.com','kensen','customer'),
+(3,'robin@somewhere.com','batman','employee');
+UNLOCK TABLES;
+INSERT INTO `order` VALUES(1, 5, 10, 5, 1);
+commit;
